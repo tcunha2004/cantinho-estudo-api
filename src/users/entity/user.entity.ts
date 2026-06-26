521e10553be6e460efd8ec96e544d0b082a3e665
tcunha2004 <tcunha2004@gmail.com>
2026-06-26 15:47:45 -0300

    feat: user entity

---
+++ src/users/entity/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+type UserRole = 'admin' | 'professor' | 'student';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
+  name: string;
+
+  @Column({
+    name: 'email',
+    length: 150,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({ name: 'password', length: 100, nullable: false })
+  password: string;
+
+  @Column({ name: 'role', nullable: false })
+  role: UserRole;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
